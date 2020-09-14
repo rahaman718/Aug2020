8f155bd4b2ab008b8d4fbf240403955b34357d47
taltekc@gmail.com <taltekc@gmail.com>
2020-09-13 21:05:51 -0400

    Aug20 first commit

---
+++ src/package1/ArrayListConcept.java
@@ -0,0 +1,27 @@
+package package1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListConcept {
+
+	public static void main(String[] args) {
+		
+		
+		List <String> st=new ArrayList<String>();
+		st.add("stt");
+		st.add("cc");
+		st.add("dd");
+		
+		System.out.println(st.get(1));
+		System.out.println(st);
+		
+		for(int i=0;i<st.size();i++){
+			System.out.println(st.get(i));
+		}
+		
+		
+
+	}
+
+}
